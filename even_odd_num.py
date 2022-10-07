ae9751460dd61305e5847de9b5576f2336da5dd2
Milan Biranwar <milanbiranwar06@gmail.com>
2022-10-07 19:52:28 +0530

    Greeting Message

---
+++ even_odd_num.py
@@ -0,0 +1,9 @@
+def even_odd_num():
+    num = int(input("Enter a number to check it is even or odd"))
+    if num % 2 == 0:
+        print(num, "is a even number")
+    else:
+        print(num, "is a odd number")
+
+
+even_odd_num()
